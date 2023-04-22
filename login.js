5cf9588a89a05bb469b0fe9e2f9775efc10b8a11
Francisco R. M. Porfírio <80773101+f-rxfxel@users.noreply.github.com>
2023-04-22 16:58:53 -0300

    Add files via upload

--- login.js
+++ login.js
@@ -2,6 +2,7 @@ const submit = document.getElementById("submit")
 const ra = document.getElementById("ra")
 const senha = document.getElementById("senha")
 const incorrectCredentials = document.getElementById("incorrectCredentials")
+const univas = document.getElementById("univas")
 
 // verificação de credenciais
 let raIV = "123"
@@ -11,16 +12,16 @@ submit.onclick = () => {
     //se os dados inseridos estiverem errados:
     if(verificarInputs(ra.value, senha.value)) {
         // usuário correto
-        incorrectCredentials.style.display = "none"
-        document.getElementById("univas").classList.add("mb-5")
-        document.getElementById("univas").classList.remove("mb-3")
+        incorrectCredentials.classList.add("d-none")
+        univas.classList.add("mb-lg-4")
+        univas.classList.remove("mb-lg-0")
         window.location.href = "cracha.html";
     } else {
         // usuário incorreto
-        incorrectCredentials.style.display = "block"
-        document.getElementById("univas").classList.remove("mb-5")
-        document.getElementById("univas").classList.add("mb-3")
-        incorrectCredentials.classList.add("mb-3")
+        incorrectCredentials.classList.remove("d-none")
+        incorrectCredentials.classList.add("d-block")
+        univas.classList.remove("mb-lg-4")
+        univas.classList.add("mb-lg-0")
         senha.classList.add("is-invalid")
         ra.classList.add("is-invalid")
     }
